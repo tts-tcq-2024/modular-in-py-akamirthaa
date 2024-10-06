0f653e44e0d8220b4a0d23dfdfcc7c6f33f64c76
akamirthaa <ak.amirthaa@gmail.com>
2024-10-07 00:00:11 +0530

    Reference manual

---
+++ ReferenceManual.py
@@ -0,0 +1,9 @@
+from ColorCodecopy import get_color_from_pair_number
+from ColorCodecopy import MAJOR_COLORS_LIST, MINOR_COLORS_LIST
+
+def print_color_code():
+    number_of_combinations = len(MAJOR_COLORS_LIST) * len(MINOR_COLORS_LIST)
+    for pair_number in range(1,number_of_combinations+1):
+        print(f"{pair_number} -----> {get_color_from_pair_number(pair_number)}")
+
+
