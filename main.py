5bf2c2c0902e4b409d6c16335d2071594f43566f
akamirthaa <ak.amirthaa@gmail.com>
2024-10-07 09:49:42 +0530

    new main.py

--- main.py
+++ main.py
@@ -1,22 +1,8 @@
-from ColorCode import get_color_from_pair_number
-from ColorCode import get_pair_number_from_color
 from ReferenceManual import print_color_code
 
-def test_number_to_pair(pair_number, expected_major_color, expected_minor_color):
-  major_color, minor_color = get_color_from_pair_number(pair_number)
-  assert(major_color == expected_major_color)
-  assert(minor_color == expected_minor_color)
-
-def test_pair_to_number(major_color, minor_color, expected_pair_number):
-  pair_number = get_pair_number_from_color(major_color, minor_color)
-  assert(pair_number == expected_pair_number)
+# Color code reference manual
+def main():
+  print_color_code()
 
 if __name__ == '__main__':
-  test_number_to_pair(4, 'White', 'Brown')
-  test_number_to_pair(5, 'White', 'Slate')
-  test_pair_to_number('Black', 'Orange', 12)
-  test_pair_to_number('Violet', 'Slate', 25)
-  test_pair_to_number('Red', 'Orange', 7)
-  print('Test Done :)')
-  print("Color code reference below ")
-  print_color_code()
+  main()
